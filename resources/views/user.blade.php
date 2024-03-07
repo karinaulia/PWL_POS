0777f75bb2db180ea4120af41a79f3e2cb00ff75
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-03-07 13:37:48 +0700

    Pertemuan 4 - Praktikum 2.1

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,16 +8,16 @@
         <table border="1" cellpading="2" cellspacing="0">
             <tr>
                 <th>ID</th>
-                <th>Usernma</th>
+                <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
             @foreach ($data as $d)
             <tr>
-                <td>{{$d->user_id}}</td>
-                <td>{{$d->username}}</td>
-                <td>{{$d->nama}}</td>
-                <td>{{$d->level_id}}</td>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
             </tr>
             @endforeach
         </table>
