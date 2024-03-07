c0b40b22135ce5c2a7dd7eb8460814f83781c744
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-03-07 16:01:34 +0700

    Pertemuan 4 - Praktikum 2.7

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -6,17 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class LevelModel extends Model
 {
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    public function level(): BelongsTo
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+        return $this->belongsTo(UserModel::class);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,6 +11,9 @@
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
+                <th>Kode Level</th>
+                <th>Nama Lebel</th>
+                <th>Aksi</th>
             </tr>
             @foreach ($data as $d)
             <tr>
@@ -18,6 +21,8 @@
                 <td>{{$d->username}}</td>
                 <td>{{$d->nama}}</td>
                 <td>{{$d->level_id}}</td>
+                <td>{{$d->level->level_kode}}</td>
+                <td>{{$d->level->level_nama}}</td>
                 <td><a href="user/ubah/{{$d->user_id}}">Ubah</a> | <a href="user/hapus/{{$d->user_id}}">Hapus</a></td>
             </tr>
             @endforeach
