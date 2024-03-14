893194fdd0dfa1a21aea94abc92a4cdd2cca43bd
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-03-14 15:13:02 +0700

    Pertemuan 5 - Praktikum 2

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -2,16 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KategoriModel extends Model
 {
-    use HasFactory;
+    protected $table = 'm_kategori';
+    protected $primaryKey = 'kategori_id';
 
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
+    protected $fillable = ['kategori_kode', 'kategori_nama'];
 
     public function barang(): HasMany
     {
