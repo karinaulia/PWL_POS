5694784674f76abc676a8445def2ddfdde4f1866
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-03-28 15:16:22 +0700

    Pertemuan 7 - Praktikum 4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.template')
+
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
@@ -9,15 +10,35 @@
         </div>
         <div class="card-body">
             @if (session('success'))
-                <div class="alert alert-success">{{ session('success')}}</div>
+                <div class="alert alert-success">{{ session('success') }}</div>
             @endif
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            @if (session('error'))
+                <div class="alert alert-error">{{ session('error') }}</div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter: </label>
+                        <div class="col-3">
+                            <select class="form-control" name="level_id" id="level_id" required>
+                                <option value="">-- Semua --</option>
+                                @foreach ($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table-bordered table-striped table-hover table-sm table" id="table_user">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Username</th>
                         <th>Nama</th>
-                        <th>Level Pengguna</th>
+                        <th>Level
+                            Pengguna</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -25,44 +46,52 @@
         </div>
     </div>
 @endsection
+
 @push('css')
 @endpush
 @push('js')
     <script>
         $(document).ready(function() {
             var dataUser = $('#table_user').DataTable({
-                serverSide: true, // serverSide: true, jika ingin menggunakan server
+                serverSide: true,
                 ajax: {
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
-                    "type": "POST"
+                    "type": "POST",
+                    "data": function(d) {
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns: [{
-                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
+                    data: "DT_RowIndex",
                     className: "text-center",
                     orderable: false,
                     searchable: false
                 }, {
                     data: "username",
                     className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
+                    orderable: true,
+                    searchable: true
                 }, {
                     data: "nama",
                     className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
+                    orderable: true,
+                    searchable: true
                 }, {
                     data: "level.level_nama",
                     className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
+                    orderable: false,
+                    searchable: false
                 }, {
                     data: "aksi",
                     className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
+                    orderable: false,
+                    searchable: false
                 }]
+            });
+
+            $('#level_id').on('change', function() {
+                dataUser.ajax.reload();
             });
         });
     </script>
